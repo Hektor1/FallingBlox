6bb9ca829a641509fd15f0e162483137432ce8c5
Kerk <dkerk@emea.corpdir.net>
2020-09-29 10:36:31 +0200

    Class-Tests
    some c#-Tests to get accustomed with CustomClasses

---
+++ BloxVR/Assets/Tests/UsingClasses/BloxClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public class BloxClass
+{
+   
+
+    public string elementName;
+    public GameObject[] elementArray;
+    public Color bloxColor;
+    
+
+    public BloxClass()
+    {
+    }
+
+    public BloxClass(string thisName)
+    {
+        this.elementName = thisName;
+    }
+
+    public BloxClass(string thisName, GameObject[] thisElementArray, Color thisColor)
+    
+    {
+        this.elementName = thisName;
+        this.elementArray = thisElementArray;
+        this.bloxColor = thisColor;
+    }
+
+
+}
